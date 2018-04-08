179f930826f0fdd65065c0c606cfea5d89017791
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 18:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deleteFromObjectByKey(object,key){
   delete newObject.key
   return newObject[key]
 }
-function destructivelyDeleObjectWithKey(object,key){
+function destructivelyDeleteFromObjectByKey(object,key){
   var newObject = Object.assign({}, object)
   return object
 }
