77cfe955e8beee550e83f7f4dbbb6603548636a0
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-08 18:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,3 @@ var recipes = {}
 function updateObjectWithKeyAndValue (object, key, value){
   return Object.assign({}, object, {[key] : value})
 }
-function updateObjectWithKeyAndValue (object,key,value){
-  var object = {}
-  object[key] = value
-  return object
-}
